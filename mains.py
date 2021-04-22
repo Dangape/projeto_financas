59444534a399b393e361acceae6d829ccfbac55d
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-04-22 17:58:36 -0300

    Update mains.py

--- mains.py
+++ mains.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import numpy as np
 
+#Load data
 dados_elementos = pd.read_excel("Dados/estrategia_con_1_0.xlsx", sheet_name="elementos_con")
 dados_indicadores = pd.read_excel("Dados/estrategia_con_1_0.xlsx", sheet_name="indicadores_con")
 
-dados = pd.merge(dados_elementos,dados_indicadores, on=["cd_cvm","dt_fim_exerc"])
+dados = pd.merge(dados_elementos,dados_indicadores, on=["cd_cvm","dt_fim_exerc"]) #merge data
 dados = pd.DataFrame(dados)
 print(dados)
 print(dados.columns)
-dados = dados.drop(columns=["denom_cia_x","cnpj_cia_x","denom_cia_y","cnpj_cia_y"])
-dados =dados.set_index("cd_cvm")
+dados = dados.drop(columns=["denom_cia_x","cnpj_cia_x","denom_cia_y","cnpj_cia_y"]) #drop company name and cnpj column
+dados =dados.set_index("cd_cvm") #set CVM code as index
 print(dados)
 
+#save new data do csv
 dados.to_csv("dados.csv", index=True)
