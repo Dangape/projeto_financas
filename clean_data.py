952f2c31cccb7092bc71b320bc357a72f4db9b3a
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-04-23 00:39:30 -0300

    Cleaning data

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import datetime as dt
+
 
 #Load data
 dados_elementos = pd.read_excel("Dados/estrategia_con_1_0.xlsx", sheet_name="elementos_con")
@@ -10,7 +12,8 @@ dados = pd.DataFrame(dados)
 print(dados)
 print(dados.columns)
 dados = dados.drop(columns=["denom_cia_x","cnpj_cia_x","denom_cia_y","cnpj_cia_y"]) #drop company name and cnpj column
-dados =dados.set_index("cd_cvm") #set CVM code as index
+#dados =dados.set_index("cd_cvm") #set CVM code as index
+dados['dt_fim_exerc']=pd.to_datetime(dados['dt_fim_exerc'], format='%m/%d/%Y').dt.year
 print(dados)
 
 #save new data do csv
