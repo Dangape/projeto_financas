b6ea0ce1ddbd1385a425463c8f4638697d148d37
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-04-30 02:56:14 -0300

    removed november

--- build.py
+++ build.py
@@ -15,49 +15,55 @@ file = prefix + filename + type + year + extention
 
 data = pd.read_csv("Data/dfp_cia_aberta_BPA_con_2010.csv",sep=";",engine="python",converters={"VL_CONTA":str})
 
-print(data.head())
 
 data.columns = data.columns.str.lower() #lowcase all headers
-data['dt_fim_exerc']=pd.to_datetime(data['dt_fim_exerc'], format='%Y-%m-%d').dt.year #extract year
+data['dt_fim_exerc']=pd.to_datetime(data['dt_fim_exerc'], format='%Y-%m-%d') #extract year
 data = data.drop(columns = ["cnpj_cia","dt_refer","denom_cia","grupo_dfp","moeda","escala_moeda","ordem_exerc",
                             "versao","st_conta_fixa","ds_conta"])#remove extra columns
+data["year"] = data["dt_fim_exerc"].dt.year
+data = data[data["year"]==2010] #remove year 2009
+data["month"] = data["dt_fim_exerc"].dt.month
+data = data[data["month"]==12]
+print(data)
 
 #Checking data types and converting them
-print(data.dtypes)
+# print(data.dtypes)
 data["cd_cvm"] = data['cd_cvm'].astype(str)
 data["cd_conta"] = data["cd_conta"].astype(str)
 data["vl_conta"] = data.vl_conta.astype(float) #mesma coisa que dividir por 10bi?
 
-print(data.dtypes)
+# print(data.dtypes)
 
-final = data[data["cd_conta"]=="1"] #ativo total
+final = data[data["cd_cvm"]=="1023"] #ativo total
+print("1.08" not in final.values)
 
 #ativo_financeiro_2010 = data.loc[(data["cd_cvm"]==)]
 print(final)
 
 unique_cvm = data.cd_cvm.unique()
 unique_account = data.cd_conta.unique()
-unique_year = [2009,2010]
 
-# print(unique_cvm)
-# print(unique_account)
+#
+print(len(unique_cvm))
+print(len(unique_account))
 
-for year in unique_year:
-    for cvm in unique_cvm:
-        for account in unique_account:
-            if account in data[(data["cd_cvm"]==str(cvm))&(data["dt_fim_exerc"]==year)].values:
-                print("ja tem a conta",account,"na empresa",cvm)
-            else:
-                new_row = {'cd_cvm': str(cvm), 'dt_fim_exerc': year, 'cd_conta': str(account), 'vl_conta': float(0)}
-                data = data.append(new_row,ignore_index=True)
-                print("linha adicionada, conta:",account,"na empresa",cvm)
+
+
+for cvm in tqdm(unique_cvm):
+    for account in unique_account:
+        if account in data[(data["cd_cvm"]==str(cvm))].values:
+            #print("ja tem a conta",account,"na empresa",cvm)
+            pass
+        elif account not in data[(data["cd_cvm"]==str(cvm))].values:
+            new_row = {'cd_cvm': str(cvm), 'dt_fim_exerc': int(2010), 'cd_conta': str(account), 'vl_conta': float(0)}
+            data = data.append(new_row,ignore_index=True)
+            #print("linha adicionada, conta:",account,"na empresa",cvm)
 
 first = data[data["cd_cvm"]=="1023"]
-second = data[data["cd_cvm"] =="21091"]
+second = data[data["cd_cvm"] =="14206"]
 print(len(first))
 print(len(second))
 
-data.to_pickle("corrected_accounts.pkl")
+data.to_pickle("corrected_accounts.pkl") #save corrected dataframe
 data = pd.read_pickle("corrected_accounts.pkl")
 print(data)
-
---
+++ create_final_data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+#load data
+data = pd.read_pickle("corrected_accounts.pkl")
+#data = data[data["dt_fim_exerc"]==2010]
+#data = data.reset_index(drop=True)
+
+
+#Calculo ativo_total
+ativo_total = data[data["cd_conta"]=="1"]
+
+#Calculo Ativo financeiro
+contas = ativo_total
+print(contas)
+first = data[data["cd_conta"]=="1.02.01.01"]
+first = first.sort_values(by=["cd_cvm"])
+first = first.reset_index(drop=True)
+print(first.duplicated(subset=['cd_cvm']).any())
+print(first[first["cd_cvm"].duplicated()==True])
+print(first[first["cd_cvm"]=="21199"])
+second = data[data["cd_conta"]=="1.01.02"]
+second = second.sort_values(by=["cd_cvm"])
+second = second.reset_index(drop=True)
+print(second.duplicated(subset=['cd_cvm']).any())
+#contas["ativo_financeiro"] = second["vl_conta"].values + first["vl_conta"].values
+print(len(first.cd_cvm.unique()))
+print(len(second.cd_cvm.unique()))
+print(second)
+print(first)
+for i in first.cd_cvm.values:
+    if i in second.cd_cvm.values:
+        #print(i,"ta aqui")
+        pass
+    else:
+        print(i,"faltando!!!!!!!!!")
+# final = pd.DataFrame({"cd_cvm":ativo_total["cd_cvm"],"dt_fim_exerc":ativo_total["dt_fim_exerc"],"ativo_total":ativo_total["vl_conta"],
+#                       "ativo_financeiro":})
+# final = final.sort_values(by=["cd_cvm"])
+# final = final.reset_index(drop=True)
+# print(final)
+
+
+#print(data[(data["cd_cvm"]==str(22497))&(data["dt_fim_exerc"]==2010)].values)
