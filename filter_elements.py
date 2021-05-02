fad77ff66aed6d2b8803261c54f65712a98d444a
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-05-01 22:45:09 -0300

    Update filter_elements.py

--- filter_elements.py
+++ filter_elements.py
@@ -19,11 +19,11 @@ data.columns = data.columns.str.lower() #lowcase all headers
 data['dt_fim_exerc']=pd.to_datetime(data['dt_fim_exerc'], format='%Y-%m-%d') #extract year
 
 data["year"] = data["dt_fim_exerc"].dt.year
-data = data[data["year"]==2010] #remove year 2009
+data = data[data["year"]==2010] #remove ano de 2009
 data["month"] = data["dt_fim_exerc"].dt.month
-data = data[data["month"]==12]
+data = data[data["month"]==12] #remove mes de novembro (11)
 data = data.drop(columns = ["cnpj_cia","dt_refer","denom_cia","grupo_dfp","moeda","escala_moeda","ordem_exerc",
-                            "versao","st_conta_fixa","ds_conta","dt_fim_exerc","month"])#remove extra columns
+                            "versao","st_conta_fixa","ds_conta","dt_fim_exerc","month"]) #remove extra columns
 print(data)
 
 #Checking data types and converting them
@@ -44,23 +44,23 @@ unique_cvm = data.cd_cvm.unique()
 unique_account = data.cd_conta.unique()
 
 print(len(unique_cvm))
-print(len(unique_account))
+print(unique_account)
 
-for cvm in tqdm(unique_cvm):
-    for account in unique_account:
-        if account in data[(data["cd_cvm"]==str(cvm))].values:
-            #print("ja tem a conta",account,"na empresa",cvm)
-            pass
-        elif account not in data[(data["cd_cvm"]==str(cvm))].values:
-            new_row = {'cd_cvm': str(cvm), 'year': int(2010), 'cd_conta': str(account), 'vl_conta': float(0)}
-            data = data.append(new_row,ignore_index=True)
-            #print("linha adicionada, conta:",account,"na empresa",cvm)
-
-first = data[data["cd_cvm"]=="1023"]
-second = data[data["cd_cvm"] =="14206"]
-print(len(first))
-print(len(second))
-
-data.to_pickle("corrected_accounts.pkl") #save corrected dataframe
-data = pd.read_pickle("corrected_accounts.pkl")
-print(data)
+# for cvm in tqdm(unique_cvm):
+#     for account in unique_account:
+#         if account in data[(data["cd_cvm"]==str(cvm))].values:
+#             #print("ja tem a conta",account,"na empresa",cvm)
+#             pass
+#         elif account not in data[(data["cd_cvm"]==str(cvm))].values:
+#             new_row = {'cd_cvm': str(cvm), 'year': int(2010), 'cd_conta': str(account), 'vl_conta': float(0)}
+#             data = data.append(new_row,ignore_index=True)
+#             #print("linha adicionada, conta:",account,"na empresa",cvm)
+#
+# first = data[data["cd_cvm"]=="1023"]
+# second = data[data["cd_cvm"] =="14206"]
+# print(len(first))
+# print(len(second))
+#
+# data.to_pickle("corrected_accounts.pkl") #save corrected dataframe
+# data = pd.read_pickle("corrected_accounts.pkl")
+# print(data)
